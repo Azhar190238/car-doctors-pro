cf45b42c172c755e47c025f3ba2c974a402f02b7
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-02 14:58:05 +0600

    Alhamdulillah updated

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -59,7 +59,3 @@ const handler = NextAuth({
 
 export { handler as GET, handler as POST }
 
-
-//         //     clientSecret: process.env.GOOGLE_CLIENT_SECRET
-//         // }),
-//         // GitHubProvider({
