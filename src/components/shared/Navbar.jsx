77b0171b882a9226eb4f031f5e41e49cbf4d6275
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-02 14:35:05 +0600

    Alhamdulillah updated

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -121,9 +121,7 @@
 
 //                 <label className="swap swap-rotate">
 //                     {/* this hidden checkbox controls the state */}
-//                     <input
-//                         type="checkbox"
-//                         className="theme-controller"
+
 
 
 
