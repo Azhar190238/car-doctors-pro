ea8e0d71749cc8afd6b570a3b124f70d6e2ebf3d
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-02 14:48:25 +0600

    Alhamdulillah updated

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -82,9 +82,6 @@
 //                         label="Email"
 //                         name="email"
 //                         rules={[{ required: true, message: 'Please input your email!' }]}
-//                     >
-//                         <Input 
-//                             type="email" 
 
 
 "use client";
