7e534f3650199c43613a1ec506dff9b79e3bc7d3
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-02 14:51:52 +0600

    Alhamdulillah updated

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -13,52 +13,6 @@
 //         console.log('Failed:', errorInfo);
 //     };
 
-//     const handleLogin = async (values) => {
-//         const email = values.email;
-//         const password = values.password;
-//         console.log('received');
-//         const resp = await signIn('credentials', {
-//             email,
-//             password,
-//             redirect: false
-//         });
-//         console.log(resp);
-//     };
-
-//     return (
-//         <section className='mt-5'>
-//             <div className='flex flex-col lg:flex-row items-center'>
-//                 <div className='flex-1'>
-//                     <Image src='/assets/images/login/login.svg' width={540} height={640} alt='login' />
-//                 </div>
-//                 <div className='flex-1'>
-//                     <div className='ml-5 lg:ml-20 border px-10 py-5'>
-//                         <h1 className='lato text-5xl text-black font-bold text-center'>Login</h1>
-//                         <Form
-//                             // onFinish={handleLogin1}
-//                             initialValues={{ remember: true }}
-//                             onFinishFailed={onFinishFailed}
-//                             autoComplete="off"
-//                         >
-
-//                             <div className='mt-10'>
-//                                 <p className='lato text-xl'>Email:</p>
-//                                 <Form.Item className=''
-//                                     name="email"
-//                                     rules={[
-//                                         { required: true, type: 'email' },
-//                                     ]}
-//                                 >
-//                                     <Input className='border border-[#FF3811] p-3' placeholder='Email' />
-//                                 </Form.Item>
-//                             </div>
-//                             <div className='mt-10'>
-//                                 <p className='lato text-xl'>Password:</p>
-//                                 <Form.Item
-//                                     name="password"
-
-
-
 "use client";
 import React from 'react';
 import Image from 'next/image';
