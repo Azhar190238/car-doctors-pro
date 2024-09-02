8896156f4210a84e52bac5dd846c6b0d7a30d0c5
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-02 14:52:08 +0600

    Alhamdulillah updated

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,5 @@
 // "use client"
 // import Image from 'next/image';
-// import React from 'react';
-// import { signIn } from 'next-auth/react';
-// import { Button, Form, Input } from 'antd';
-// import { FaFacebook, FaLinkedin } from "react-icons/fa";
-
 
 
 "use client";
