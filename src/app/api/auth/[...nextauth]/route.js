1feb7ad0a0cba5cca334b7a708fe2474426fe295
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-02 14:54:09 +0600

    Alhamdulillah updated

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -64,9 +64,7 @@ export { handler as GET, handler as POST }
 
 
 
-// const handler = NextAuth({
-//     // secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
-//     session: {
+
 //         strategy: "jwt",
 //         maxAge: 30 * 24 * 60 * 60,
 //     },
