79a695d07ce7b1da467415413c4a71dbed5db6c5
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-04 18:56:17 +0600

    Alhamdulillah updated

--- src/components/HomePage/Services.jsx
+++ src/components/HomePage/Services.jsx
@@ -30,9 +30,6 @@ const Services = async () => {
 
 export default Services;
 
-//                 <p className='max-w-[700px] text-[16px] text-gray-500 text-center'>
-//                     The majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.
-//                 </p>
 //             </div>
 //             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
 //                 {providers.map((service) => (
