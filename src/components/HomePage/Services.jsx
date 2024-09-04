328797a3630184e3e69a54d3ef509a7bd65191a2
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-04 18:55:06 +0600

    Alhamdulillah updated

--- src/components/HomePage/Services.jsx
+++ src/components/HomePage/Services.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ServicseCard from '../cards/ServicseCard';
 import { getService } from '@/services/getServices';
 
-// const getServices = async () => {
-//     const res = await fetch("http://localhost:3000/services/api/get-all")
-//     const data = await res.json()
-//     return data;
-// }
+
 
 // const services = await getService();
 const Services = async () => {
