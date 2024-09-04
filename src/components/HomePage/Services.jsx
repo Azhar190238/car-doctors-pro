a111ca05db74465a43a6203050487e29f909d136
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-04 18:56:11 +0600

    Alhamdulillah updated

--- src/components/HomePage/Services.jsx
+++ src/components/HomePage/Services.jsx
@@ -30,10 +30,6 @@ const Services = async () => {
 
 export default Services;
 
-
-//             <div className='flex flex-col space-y-6 items-center text-center mb-10'>
-//                 <h1 className='font-bold text-xl text-[#FF3811]'>Services</h1>
-//                 <h2 className='font-bold text-2xl md:text-5xl'>Our Service Area</h2>
 //                 <p className='max-w-[700px] text-[16px] text-gray-500 text-center'>
 //                     The majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.
 //                 </p>
